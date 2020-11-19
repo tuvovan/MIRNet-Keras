79647a4e721f17b4c15ce5bae933c907ca510902
tuvovan <vovantu.hust@gmail.com>
2020-11-19 17:28:00 +0900

    change train file

--- train_denoise.py
+++ train_denoise.py
@@ -14,12 +14,12 @@ from utils import *
 train_ds = SSID(subset='train').dataset(repeat_count=1)
 valid_ds = SSID(subset='valid').dataset(repeat_count=1)
 
-test_path = 'test/denoise'
+test_path = 'test/denoise/'
 test_img_paths = sorted(
     [
         os.path.join(test_path, fname)
         for fname in os.listdir(test_path)
-        if fname.endswith(".png")
+        if fname.endswith(".PNG")
     ]
 )
 
@@ -32,25 +32,26 @@ def train(config):
     model = Model(inputs=x, outputs=out)
     model.summary()
 
-    early_stopping_callback = keras.callbacks.EarlyStopping(monitor="loss", patience=10)
+    early_stopping_callback = keras.callbacks.EarlyStopping(monitor="psnr_denoise", patience=10)
     checkpoint_filepath = config.checkpoint_filepath
 
+    
     model_checkpoint_callback = keras.callbacks.ModelCheckpoint(
-        checkpoint_filepath + '/best.h5',
-        monitor="loss",
-        mode="min",
+        checkpoint_filepath + f'{{epoch:02d}}_{{psnr_denoise:.2f}}.h5',
+        monitor="psnr_denoise",
+        mode="max",
         save_best_only=True,
-        period=config.checkpoint_ep
+        period=1
     )
 
-    callbacks = [ESPCNCallback(test_img_paths), early_stopping_callback, model_checkpoint_callback]
+    callbacks = [ESPCNCallback(test_img_paths, mode=config.mode, checkpoint_ep=config.checkpoint_ep), early_stopping_callback, model_checkpoint_callback]
     loss_fn = keras.losses.MeanSquaredError()
     optimizer = keras.optimizers.Adam(learning_rate = config.lr)
 
     epochs = config.num_epochs
 
     model.compile(
-        optimizer=optimizer, loss=loss_fn,
+        optimizer=optimizer, loss=loss_fn, metrics=[psnr_denoise]
     )
 
     model.fit(
@@ -64,7 +65,7 @@ if __name__ == "__main__":
 
 	# Input Parameters
 	parser.add_argument('--lr', type=float, default=1e-4)
-	parser.add_argument('--gpu', type=int, default=0)
+	parser.add_argument('--gpu', type=str, default='0')
 	parser.add_argument('--grad_clip_norm', type=float, default=0.1)
 	parser.add_argument('--num_epochs', type=int, default=100)
 	parser.add_argument('--train_batch_size', type=int, default=32)
@@ -72,10 +73,11 @@ if __name__ == "__main__":
 	parser.add_argument('--checkpoint_filepath', type=str, default="weights/denoise/")
 	parser.add_argument('--num_rrg', type=int, default= 3)
 	parser.add_argument('--num_mrb', type=int, default= 2)
+	parser.add_argument('--mode', type=str, default= 'denoise')
 
 	config = parser.parse_args()
 
-	if not os.path.exists(config.checkpoints_folder):
-		os.mkdir(config.checkpoints_folder)
+	if not os.path.exists(config.checkpoint_filepath):
+		os.mkdir(config.checkpoint_filepath)
 
 	train(config)
